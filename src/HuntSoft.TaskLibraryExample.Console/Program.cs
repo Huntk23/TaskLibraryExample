4defd00864a55b2b8a402ecb699bcbe40590f6b7
Kelby Hunt <huntk20@gmail.com>
2023-01-25 10:40:52 -0800

    Initial commit

---
+++ src/HuntSoft.TaskLibraryExample.Console/Program.cs
@@ -0,0 +1,30 @@
+﻿using HuntSoft.TaskLibraryExample;
+
+var worker = new Worker();
+
+Console.WriteLine("Starting library background work");
+
+await worker.Start();
+
+Console.WriteLine("Retrieving data every 15 seconds in the console or press 'Esc' key to end the retrieval and stop the library from working in the background");
+
+await Task.Delay(250); // Lets give worker.Start() 250 milliseconds to retrieve the first result
+
+do
+{
+    while (!Console.KeyAvailable)
+    {
+        Console.WriteLine("Result received at {0}: {1}", DateTime.Now, await worker.GetData());
+        await Task.Delay(TimeSpan.FromSeconds(15)); // When we press Esc key, we will get stuck here until the delay finishes - outside the scope of the question
+    }
+} while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+
+Console.WriteLine("Calling Stop() that will cancel the task retrieval process within the library");
+
+await worker.Stop();
+
+Console.WriteLine("The library has shutdown the retrieval process, press any key to end");
+
+worker.Dispose();
+
+Console.ReadKey();
---
+++ src/HuntSoft.TaskLibraryExample/Worker.cs
@@ -0,0 +1,66 @@
+﻿namespace HuntSoft.TaskLibraryExample;
+
+public class Worker : IDisposable
+{
+    private readonly CancellationTokenSource _cancellationTokenSource = new();
+
+    private readonly HttpClient _httpClient = new();
+
+    private string? _result;
+
+    public async Task Start(TimeSpan? refreshInterval = null)
+    {
+        var refreshIntervalLocal = refreshInterval ?? TimeSpan.FromMinutes(10); // Default is 10 minutes or pass in a shorter TimeSpan - be warned, by making this something shorter than a second will hit rate limits and other issues
+
+        // TODO: TimeSpan validation
+
+        // Start the work in a Task - depending on need and longevity, Task.Run() can also be used with minimal issue
+        await Task.Factory.StartNew(async () =>
+        {
+            try
+            {
+                while (true)
+                {
+                    // Retrieve data from a website per requester asked
+                    _result = await _httpClient.GetStringAsync("https://jsonplaceholder.typicode.com/posts", _cancellationTokenSource.Token); // If the request to the server takes too long, pass in the token so Stop() will cancel this
+
+                    // If more work is done after the GetStringAsync call, consider using the following when needed to break out of this while loop quicker than GetString or Task.Delay cancels
+
+                    //if (_cancellationTokenSource.IsCancellationRequested)
+                    //{
+                    //    _cancellationTokenSource.Token.ThrowIfCancellationRequested();
+                    //}
+
+                    // while (!_cancellationTokenSource.IsCancellationRequested) works too, it really depends on how the internal logic of the loop works
+                    // {
+                    //    // Work
+                    // }
+
+                    await Task.Delay(refreshIntervalLocal, _cancellationTokenSource.Token); // Cancellation token is passed into the delay in case Stop() is called during the delay
+                }
+            }
+            catch (TaskCanceledException)
+            {
+                Console.WriteLine("Task has been canceled");
+            }
+        }, _cancellationTokenSource.Token);
+    }
+
+    public Task Stop()
+    {
+        _cancellationTokenSource.Cancel(); // Tell all the methods utilizing our token sources token to cancel
+
+        return Task.CompletedTask;
+    }
+
+    public Task<string?> GetData()
+    {
+        return Task.FromResult(_result); // Not entirely safe if you have many threads running the same Worker instance and calling this method
+    }
+
+    public void Dispose()
+    {
+        _cancellationTokenSource.Dispose();
+        _httpClient.Dispose();
+    }
+}
